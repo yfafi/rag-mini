365ee385320ffb2a1b241b3f0667df40a4dd8e5a
yfafi@redhat.com <yfafi@myges.fr>
2025-08-11 20:36:09 +0200

    Ajout d'un fichier + Ajout des creds

--- rag_chain.py
+++ rag_chain.py
@@ -1,6 +1,8 @@
 import os
 import pathlib
 from typing import List
+from dotenv import load_dotenv
+load_dotenv()
 
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
